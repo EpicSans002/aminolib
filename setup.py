8b897da36f5dddedea7b352acc3c2ed3ffe4d9cd
EpicSans002 <71330540+EpicSans002@users.noreply.github.com>
2022-06-10 09:10:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 with open("README.md", "r") as stream:
     long_description = stream.read()
-setuptools.setup(name="aminolib",license = "MIT",version="0.0.1",author="@kingzero#6580",description="Library for Amino",long_description=long_description,long_description_content_type = "text/markdown",install_requires=["datetime","flask","requests","json-minify"],keywords=["amino","aminolib","Lord","AminoAPI"],python_requires=">=3.6",url="https://github.com/EpicSans002/aminolib",project_urls={"aminolib": "https://github.com/EpicSans002/aminolib/issues",},
+setuptools.setup(name="aminolib",license = "MIT",version="0.0.5",author="@kingzero#6580",description="Library for Amino",long_description=long_description,long_description_content_type = "text/markdown",install_requires=["datetime","flask","requests","json-minify"],keywords=["amino","aminolib","Lord","AminoAPI"],python_requires=">=3.6",url="https://github.com/EpicSans002/aminolib",project_urls={"aminolib": "https://github.com/EpicSans002/aminolib/issues",},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
